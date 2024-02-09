f45e56c035549d4be1e2e2d1eb3dcf2ee211ab15
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2024-02-09 15:15:59 +0100

    fix duplicate registered errors

--- x/gov/types/errors.go
+++ x/gov/types/errors.go
@@ -6,12 +6,12 @@ import (
 
 // x/gov module sentinel errors
 var (
-	ErrUnknownProposal         = sdkerrors.Register(ModuleName, 2, "unknown proposal")
-	ErrInactiveProposal        = sdkerrors.Register(ModuleName, 3, "inactive proposal")
-	ErrAlreadyActiveProposal   = sdkerrors.Register(ModuleName, 4, "proposal already active")
-	ErrInvalidProposalContent  = sdkerrors.Register(ModuleName, 5, "invalid proposal content")
-	ErrInvalidProposalType     = sdkerrors.Register(ModuleName, 6, "invalid proposal type")
-	ErrInvalidVote             = sdkerrors.Register(ModuleName, 7, "invalid vote option")
-	ErrInvalidGenesis          = sdkerrors.Register(ModuleName, 8, "invalid genesis state")
-	ErrNoProposalHandlerExists = sdkerrors.Register(ModuleName, 9, "no handler exists for proposal type")
+	ErrUnknownProposal         = sdkerrors.Register(ModuleName, 12, "unknown proposal")
+	ErrInactiveProposal        = sdkerrors.Register(ModuleName, 13, "inactive proposal")
+	ErrAlreadyActiveProposal   = sdkerrors.Register(ModuleName, 14, "proposal already active")
+	ErrInvalidProposalContent  = sdkerrors.Register(ModuleName, 15, "invalid proposal content")
+	ErrInvalidProposalType     = sdkerrors.Register(ModuleName, 16, "invalid proposal type")
+	ErrInvalidVote             = sdkerrors.Register(ModuleName, 17, "invalid vote option")
+	ErrInvalidGenesis          = sdkerrors.Register(ModuleName, 18, "invalid genesis state")
+	ErrNoProposalHandlerExists = sdkerrors.Register(ModuleName, 19, "no handler exists for proposal type")
 )
